93c8aaaf1704c5192d2024a7e8ed6e3937ca29f1
GenesisSam <samuel940401@gmail.com>
2019-04-20 06:57:23 +0900

    Use pmApp.js

--- pmApp.js
+++ pmApp.js
@@ -1,14 +1,13 @@
 module.exports = {
   apps: [{
     name: "fpa-backend",
-    script: "ts-node",
-    args: "-T app/index.ts",
+    script: "app/index.ts",
     instances: "max",
     watch: true,
     ignore_watch: ["node_modules", "sqls"],
     env: {
       "production": true,
     },
-    PORT: 3000,
+    PORT: 80,
   }]
 }

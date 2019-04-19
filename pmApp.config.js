3c9250137cb74c5250fa8734fa4bb0750901ac34
GenesisSam <samuel940401@gmail.com>
2019-04-20 07:09:35 +0900

    Propelry working config.js

---
+++ pmApp.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: "fpa-backend",
+    script: "app/index.ts",
+    watch: true,
+    ignore_watch: ["node_modules", "sqls"],
+    env: {
+      production: true,
+      "NODE_ENV": true,
+      "SENDGRID_API_KEY": process.env.SENDGRID_API_KEY,
+      "MYSQL_ADDR": process.env.MYSQL_ADDR,
+      "MYSQL_USER": process.env.MYSQL_USER,
+      "MYSQL_PWD": process.env.MYSQL_PWD,
+    },
+  }]
+}

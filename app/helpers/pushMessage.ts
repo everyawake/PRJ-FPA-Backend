4da42b4c3c51b6083600c50ad87ed225e85acf87
GenesisSam <samuel940401@gmail.com>
2019-09-18 09:29:17 +0900

    Update directory name

--- app/helpers/pushMessage.ts
+++ app/helpers/pushMessage.ts
@@ -1,28 +1,36 @@
 import firebaseAdmin from "firebase-admin";
 
-const firebaseConfig = require("../../.config/finger-print-authenticate-060a38c21529.json");
+const firebaseConfig = require("../../configs/finger-print-authenticate-060a38c21529.json");
 
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(firebaseConfig),
+  credential: firebaseAdmin.credential.cert(firebaseConfig)
 });
 
-export function pushAuthRequestMessage(targetToken: string, thirdPartyName: string, socketId: string) {
+export function pushAuthRequestMessage(
+  targetToken: string,
+  thirdPartyName: string,
+  socketId: string
+) {
   const message: firebaseAdmin.messaging.Message = {
     notification: {
       title: "인증 요청",
-      body: `[${thirdPartyName}]에서 인증을 요청했습니다`,
+      body: `[${thirdPartyName}]에서 인증을 요청했습니다`
     },
     data: {
       page: "AUTH_REQUEST",
       thirdPartyName,
-      socketId,
+      socketId
     },
-    token: targetToken,
+    token: targetToken
   };
 
-  firebaseAdmin.messaging().send(message).then((res) => {
-    console.log(`Successfully GCM send: ${res}`);
-  }).catch((err) => {
-    console.log(`Failed GCM send: ${err}`);
-  })
+  firebaseAdmin
+    .messaging()
+    .send(message)
+    .then(res => {
+      console.log(`Successfully GCM send: ${res}`);
+    })
+    .catch(err => {
+      console.log(`Failed GCM send: ${err}`);
+    });
 }
